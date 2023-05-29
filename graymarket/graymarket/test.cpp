2cc0bfd9da3559a971d48a0dc48efadb08ea8583
m_bondale <m_bondale@u.pacific.edu>
2023-05-29 15:21:52 -0700

    installed_apps

---
+++ graymarket/graymarket/test.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+cout<<"hekllow rodl"<<endl;
+
+		for(int i = 0; i  < N;i ++ ) {
+
+		}
+
+
+		std::cout<<"matrix1"<<std::endl;
+		std::cout<<matrix1<<std::endl;
+
+
+	return 0;
+}
